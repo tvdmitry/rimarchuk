a98e852a83878e1145ee98c79533c69a4bb1986f
Andrets <dmitriysurkov2019@gmail.com>
2024-07-23 17:31:00 +0500

    rework productPage and components

--- src/utils/types/book.ts
+++ src/utils/types/book.ts
@@ -1,24 +1,29 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
 export interface IBookContentList {
     title: string;
+}
+
+export interface IBook {
+    contentTitle?: string;
+    contentInfo?: string;
+    contentList?: IBookContentList[];
+    buttonText?: string;
+    buttonBuy?: string;
+    price?: string;
+    descriptionPrice?: string;
+    bonus?: boolean;
 }
 
 export interface IBookBlock {
     id: string | number;
     title: string | ReactNode;
     image: string;
-    bonus?: boolean;
     book?: boolean;
     link?: boolean;
     description: string;
-    descriptionPrice?: string;
-    price?: string;
-    buttonText?: string;
-    buttonBuy?: string;
     cost?: number;
-    contentTitle?: string;
-    contentInfo?: string;
-    contentList?: IBookContentList[];
     onClick?: () => void;
+    buy?: boolean;
+    content?: IBook[];
 }
